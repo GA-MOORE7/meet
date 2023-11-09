524ffb2cc835c100eed8d143d8123178aa2dae86
Gerald Arthur Moore <gerald.moore42@gmail.com>
2023-11-09 02:31:17 +0200

    Offline capability

--- src/api.js
+++ src/api.js
@@ -1,5 +1,4 @@
 import mockData from './mock-data';
-import NProgress from "nprogress";
 
 export const getAccessToken = async () => {
   const accessToken = localStorage.getItem('access_token');
@@ -31,10 +30,7 @@ const checkToken = async (accessToken) => {
 };
 
 export const getEvents = async () => {
-  NProgress.start();
-
   if (window.location.href.startsWith('http://localhost')) {
-    NProgress.done();
     return mockData;
   }
 
@@ -46,7 +42,6 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
-      NProgress.done();
       localStorage.setItem('lastEvents', JSON.stringify(result.events));
       return result.events;
     } else return null; 
