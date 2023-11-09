f937fa964116885b75f04fd55a2150a70ba9c9d8
Gerald Arthur Moore <gerald.moore42@gmail.com>
2023-11-10 00:25:49 +0200

    Create WarningAlert notification

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -48,5 +48,12 @@ class InfoAlert extends Alert {
     }
   }
 
+  class WarningAlert extends Alert {
+    constructor(props) {
+      super(props);
+      this.color = 'rgb(255, 165, 0)'; // orange
+      this.bgColor = 'rgb(255, 213, 128)'; // light orange
+    }
+  }
 
-  export { InfoAlert, ErrorAlert };
+  export { InfoAlert, ErrorAlert, WarningAlert };
