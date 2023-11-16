83bbcf12dd9b90647527618fbe95306e981857fa
Gerald Arthur Moore <gerald.moore42@gmail.com>
2023-11-17 01:17:23 +0200

    Create CityEventsChart and EventGenresChart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from "./components/NumberOfEvents";
+import CityEventsChart from "./components/CityEventsChart";
+import EventGenresChart from "./components/EventGenresChart"; 
 import { useEffect, useState } from "react";
 import { extractLocations, getEvents } from './api';
 import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
 
 import './App.css';
-
 
 const App = () => {
   const [events, setEvents] = useState([]);
@@ -51,9 +52,11 @@ const App = () => {
           setCurrentNOE={setCurrentNOE} 
           setErrorAlert={setErrorAlert}
         />
-        <EventList events={events} />
-        
-               
+        <div className="charts-container">
+          <EventGenresChart events={events} />
+          <CityEventsChart allLocations={allLocations} events={events} />
+        </div>
+        <EventList events={events} />     
     </div>
   );
 }
